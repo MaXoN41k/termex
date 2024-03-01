b91351102617b138b072fbd18bb0d2b17b7067c4
MaXoN41k <90177831+MaXoN41k@users.noreply.github.com>
2024-03-01 16:03:08 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 using namespace std;
 
 int main()
 {
+  setlocale(LC_ALL, "rus");
   srand(time(0));
   double lambda;
   int count;
@@ -18,8 +20,14 @@ int main()
   cin >> count;
   cout << endl;
 
+  vector<double> res(count);
+
   double x = 0;
   double y = 0;
+
+  double x_ = 0;
+  double S_2 = 0;
+  double me = 0;
   
   /*
   double max = 0;
@@ -46,6 +54,7 @@ int main()
     cout << y <<endl;
   }
   */
+
   for (int n = 0; n < count; n++)
   {
     x = ((double)rand()) / RAND_MAX;
@@ -61,9 +70,25 @@ int main()
     {
       y = 0;
     }
-
+    res[n] = y;
+    x_ += y;
     cout << y <<endl;
   }
+
+  for (int n = 0; n < count; n++)
+  {
+    S_2 += (res[n] - (x_ / count)) * (res[n] - (x_ / count));
+  }
+
+  S_2 = S_2 / count;
+
+  cout << endl;
+  cout << "Матожидание = 0" << endl;
+  cout << "Выборочное среднее = " << x_/count << endl;
+  cout << "Отклонение от матожидания = " << (-1) * (x_ / count) << endl;
+  cout << "Дисперсия случайной величины = " << 2 / ((lambda) * (lambda)) << endl;
+  cout << "Выборочная дисперсия случайной величины = " << S_2 << endl;
+  cout << "Отклонение от дисперсии = " << abs((2 / ((lambda) * (lambda)))-S_2) << endl;
 
   return 0;
 }
